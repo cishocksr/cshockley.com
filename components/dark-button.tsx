34a0f23560bc030111df8d13278e4f29a13dea54
Chris Shockley <c.shocksr3@gmail.com>
2025-02-17 10:28:23 -0500

    more updates for deployment

--- components/dark-button.tsx
+++ components/dark-button.tsx
@@ -1,32 +1,26 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
-import { DarkIcon, LightIcon } from "@/components/icons/logos";
 import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
+import { LightIcon, DarkIcon } from "./icons/logos";
 
 export const DarkModeButton = () => {
-    const [clientLoaded, setClientLoaded] = useState(false); // Add clientLoaded state
+    const [clientLoaded, setClientLoaded] = useState(false);
     const { setTheme, theme, resolvedTheme } = useTheme();
 
     useEffect(() => {
         setClientLoaded(true);
-    },);
+    }, [setClientLoaded]);
 
     return (
         <Button
-            variant="outline"
-            size="icon"
             onClick={() => setTheme(theme === "dark" || resolvedTheme === "dark" ? "light" : "dark")}
         >
-            {clientLoaded && ( // Conditionally render icons on client-side
-                <>
-                    {theme === "dark" || resolvedTheme === "dark" ? (
-                        <LightIcon className="h-[1.2rem] w-[1.2rem]" />
-                    ) : (
-                        <DarkIcon className="h-[1.2rem] w-[1.2rem]" />
-                    )}
-                </>
+            {clientLoaded && (
+                <div className="relative size-8">
+                    {theme === "dark" ? <LightIcon /> : <DarkIcon />}
+                </div>
             )}
         </Button>
     );
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,15 +6,11 @@ export const Button = ({
     children,
     className,
     onClick,
-    variant,
-    size,
 }: {
     children?: React.ReactNode;
     className?: string;
     href?: string;
     onClick?: () => void;
-    variant?: "outline" | "default";
-    size?: "icon" | "default";
 }) => {
     return (
         <div
@@ -23,6 +19,7 @@ export const Button = ({
                 "w-full bg-white rounded-xl border border-neutral-100 dark:bg-black dark:border-white/ hover:border-neutral-200 group/btn overflow-hidden relative flex items-center justify-center",
                 className
             )}
+        // Remove as={HTMLDivElement}
         >
             <div className="absolute inset-0 dark:bg-dot-white/ bg-dot-black/" />
             <div className="relative z-40">{children}</div>
